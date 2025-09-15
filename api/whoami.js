9d77262b5f39f1b0a95bc7419fa748e5f13924a9
Ricmonpa <rmmoncada5@gmail.com>
2025-09-14 19:31:59 -0600

    deletoin whoami.js

--- api/whoami.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    project: 'blabla-pet-ai',
-    env: process.env.BLOB_READ_WRITE_TOKEN ? 'token-exists' : 'token-missing'
-  });
-}
